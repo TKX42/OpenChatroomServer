fa49bef644f546c6c790b69f03697e36046f8cab
Tristan K <tristan@kaupa.com>
2022-08-21 11:17:26 +0200

    MessageListTest fix: changed LocalDateTime to ZonedDateTime

--- src/test/java/tkx42/openchatroom/OpenChatroomServer/MessageListTest.java
+++ src/test/java/tkx42/openchatroom/OpenChatroomServer/MessageListTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import tkx42.openchatroom.OpenChatroomServer.model.Message;
 import tkx42.openchatroom.OpenChatroomServer.model.MessageList;
 
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -20,17 +20,17 @@ public class MessageListTest {
 
     @Test
     void addTestSmallerThanChunk() {
-        messageList.addMessage(new Message("Content", LocalDateTime.now(), null));
-        messageList.addMessage(new Message("Content", LocalDateTime.now(), null));
+        messageList.addMessage(new Message("Content", ZonedDateTime.now(), null));
+        messageList.addMessage(new Message("Content", ZonedDateTime.now(), null));
         assertEquals(2, messageList.getChunk(0).size());
     }
 
     @Test
     void basicChunkTest() {
-        Message msg1 = new Message("Content1", LocalDateTime.now(), null);
-        Message msg2 = new Message("Content2", LocalDateTime.now(), null);
-        Message msg3 = new Message("Content3", LocalDateTime.now(), null);
-        Message msg4 = new Message("Content4", LocalDateTime.now(), null);
+        Message msg1 = new Message("Content1", ZonedDateTime.now(), null);
+        Message msg2 = new Message("Content2", ZonedDateTime.now(), null);
+        Message msg3 = new Message("Content3", ZonedDateTime.now(), null);
+        Message msg4 = new Message("Content4", ZonedDateTime.now(), null);
         messageList.addMessage(msg1);
         messageList.addMessage(msg2);
         messageList.addMessage(msg3);
@@ -54,8 +54,8 @@ public class MessageListTest {
 
     @Test
     void outOfRangeChunkTest() {
-        messageList.addMessage(new Message("Content", LocalDateTime.now(), null));
-        messageList.addMessage(new Message("Content", LocalDateTime.now(), null));
+        messageList.addMessage(new Message("Content", ZonedDateTime.now(), null));
+        messageList.addMessage(new Message("Content", ZonedDateTime.now(), null));
         assertNull(messageList.getChunk(1));
     }
 
@@ -64,7 +64,7 @@ public class MessageListTest {
     @Test
     void notWholeChunkTest() {
         for (int i = 0; i < messageList.getChunkSize() + 10; i++) {
-            messageList.addMessage(new Message("Content", LocalDateTime.now(), null));
+            messageList.addMessage(new Message("Content", ZonedDateTime.now(), null));
         }
 
         assertEquals(16, messageList.getChunk(0).size());
@@ -75,11 +75,11 @@ public class MessageListTest {
     @Test
     void notWholeChunkTest2() {
         for (int i = 0; i < messageList.getChunkSize(); i++) {
-            messageList.addMessage(new Message("Content #" + i, LocalDateTime.now(), null));
+            messageList.addMessage(new Message("Content #" + i, ZonedDateTime.now(), null));
         }
 
-        messageList.addMessage(new Message("EXTRA #1", LocalDateTime.now(), null));
-        messageList.addMessage(new Message("EXTRA #2", LocalDateTime.now(), null));
+        messageList.addMessage(new Message("EXTRA #1", ZonedDateTime.now(), null));
+        messageList.addMessage(new Message("EXTRA #2", ZonedDateTime.now(), null));
 
         assertEquals(2, messageList.getChunk(1).size());
         assertEquals("Content #0", messageList.getChunk(1).get(0).getContent());
